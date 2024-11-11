43a0324233846ad3aac26d8d712880863e0da4e5
CodingCookiee <rzqutubawan@gmail.com>
2024-11-11 17:16:51 +0500

    slides and catcard components

---
+++ src/components/slides/Slides.jsx
@@ -0,0 +1,58 @@
+// src/components/slides/Slide.js
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+// Custom arrow components with light grey background styling
+const arrowStyle = {
+  backgroundColor: "lightgrey",
+  color:"lightgrey",
+  borderRadius: "50%",
+  padding: "30px",
+  border: "none",
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  zIndex: 1, 
+};
+
+const PrevArrow = ({ onClick }) => (
+  <button style={{ ...arrowStyle, left: "-30px" }} onClick={onClick} className="slick-prev">
+    <FaArrowLeft />
+  </button>
+);
+
+const NextArrow = ({ onClick }) => (
+  <button style={{ ...arrowStyle, right: "-10px" }} onClick={onClick} className="slick-next">
+    <FaArrowRight />
+  </button>
+);
+
+const Slide = ({ children, slidesToShow }) => {
+  const settings = {
+    slidesToShow: slidesToShow,
+    slidesToScroll: 2,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    swipeToSlide: true,
+    arrows: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
+  return (
+    <div className="slide flex justify-center p-[100px] pl-0 pr-0">
+      <div className="container w-[1400px]">
+        <Slider {...settings}>
+          {children}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
+// src/pages/Home.js
 import React from 'react';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/trustedBy';
+import Slide from '../../components/slides/Slides';
+import { cards } from '../../data.js';
+import CatCard from '../../components/cateogryCard/categoryCard';
 
 const Home = () => {
     return (
-        <div className='home'>
-        <Featured/>
-        <TrustedBy/>
+        <div className="home">
+            <Featured />
+            <TrustedBy />
+            <Slide slidesToShow={5} arrowsScroll={3}>
+                {cards.map((card) => (
+                    <CatCard key={card.id} card={card} />
+                ))}
+            </Slide>
         </div>
     );
-}
+};
 
 export default Home;
