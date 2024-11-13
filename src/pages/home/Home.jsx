f32174ab954ba3d528ac910ba4c032659858d306
CodingCookiee <rzqutubawan@gmail.com>
2024-11-13 10:56:10 +0500

    Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,8 @@ import TrustedBy from '../../components/trustedBy/trustedBy';
 import Slide from '../../components/slides/Slides';
 import { cards, projects } from '../../data.js';
 import CatCard from '../../components/cateogryCard/categoryCard';
-import Features from '../../components/features/Features.jsx';
-import ProjectCard from '../../components/projectCard/ProjectCard.jsx';
+import Features from '../../components/features/Features';
+import ProjectCard from '../../components/projectCard/ProjectCard';
 
 
 const Home = () => {
