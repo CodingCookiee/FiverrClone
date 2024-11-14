ee1db6af5087ceb95658fc6163740aa6e80531a7
CodingCookiee <rzqutubawan@gmail.com>
2024-11-14 22:03:35 +0500

    Register User Page almost finalised; the user is being registered successfully; only the img is not being uploaded

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,80 +1,171 @@
-import React from 'react';
-import ToggleSwitch from './toggleSwitch';
+import React, { useState } from "react";
+import ToggleSwitch from "./toggleSwitch";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
+import upload from "../../utils/upload";
 
 const Register = () => {
-    return (
-        <div className="register flex items-center justify-center ">
-        <form
-        className='w-[960px] flex gap-[120px] p-[100px] pl-0 pr-0'
-        >
-          <div className="left flex-1 flex flex-col gap-2.5 justify-between">
-            <h1 className='text-[#555555] font-semibold text-2xl'>Create a new account</h1>
-            <label htmlFor="username" className='color-[gray] text-[18px]'>Username</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200'
-              name="username"
-              type="text"
-              id='username'
-              placeholder="John Doe"
-             
+  const navigate = useNavigate();
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    img: "",
+    country: "",
+    isSeller: false,
+    phone: "",
+    desc: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleSeller = (e) => {
+    setUser((prev) => ({ ...prev, isSeller: e.target.checked }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = await upload(file);
+    try {
+      await newRequest.post("/auth/register", {
+        ...user,
+        img: url,
+      });
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+  return (
+    <div className="register flex items-center justify-center ">
+      <form
+        className="w-[960px] flex gap-[120px] p-[100px] pl-0 pr-0"
+        onSubmit={handleSubmit}
+      >
+        <div className="left flex-1 flex flex-col gap-2.5 justify-between">
+          <h1 className="text-[#555555] font-semibold text-2xl">
+            Create a new account
+          </h1>
+          <label htmlFor="username" className="color-[gray] text-[18px]">
+            Username<span className="text-red-500">*</span>
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            name="username"
+            type="text"
+            id="username"
+            placeholder="John Doe"
+            onChange={handleChange}
+            value={user.username}
+          />
+          <label htmlFor="email" className="color-[gray] text-[18px]">
+            Email<span className="text-red-500">*</span>
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            name="email"
+            type="email"
+            id="email"
+            placeholder="johndoe@example.com"
+            onChange={handleChange}
+            value={user.email}
+          />
+          <label htmlFor="password" className="color-[gray] text-[18px]">
+            Password<span className="text-red-500">*</span>
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            name="password"
+            type="password"
+            id="password"
+            onChange={handleChange}
+            value={user.password}
+          />
+          <label htmlFor="file" className="color-[gray] text-[18px]">
+            Profile Picture
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            type="file"
+            id="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <label htmlFor="country" className="color-[gray] text-[18px]">
+            Country<span className="text-red-500">*</span>
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            name="country"
+            type="text"
+            id="country"
+            placeholder="Usa"
+            onChange={handleChange}
+            value={user.country}
+          />
+          {error && <span className="text-red-500">{error}</span>}
+          <button
+            className="bg-[#1dbf73] hover:bg-[#10b981] transition-all text-[white]
+             p-[20px] rounded-md border-none font-medium text-[18px] cursor-pointer"
+            type="submit"
+          >
+            Register
+          </button>
+        </div>
+        <div className="right flex-1 flex flex-col gap-2.5 justify-between">
+          <h1 className="text-[#555555] font-semibold text-2xl">
+            I want to become a seller
+          </h1>
+          <div className="toggleSwitch flex items-center gap-2">
+            <label htmlFor="toggle" className="color-[gray] text-[18px]">
+              Activate the seller account
+            </label>
+            {/* Passing handleSeller as the onChange prop */}
+            <ToggleSwitch
+              checked={user.isSeller}
+              onChange={handleSeller}
+              id="toggle"
+              name="isSeller"
             />
-            <label htmlFor="email" className='color-[gray] text-[18px]'>Email</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200'
-              name="email"
-              type="email"
-              id='email'
-              placeholder="johndoe@example.com"
-             
-            />
-            <label htmlFor="password" className='color-[gray] text-[18px]'>Password</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200' name="password" type="password" id='password'/>
-            <label htmlFor="file" className='color-[gray] text-[18px]'>Profile Picture</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200' type="file" id='file' />
-            <label htmlFor="country" className='color-[gray] text-[18px]'>Country</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200'
-              name="country"
-              type="text"
-              id='country'
-              placeholder="Usa"
-             
-            />
-            <button
-            className='bg-[#1dbf73] hover:bg-[#10b981] transition-all text-[white] p-[20px] rounded-md border-none font-medium text-[18px] cursor-pointer'
-             type="submit"
-             >Register</button>
           </div>
-          <div className="right flex-1 flex flex-col gap-2.5 justify-between">
-            <h1 className='text-[#555555] font-semibold text-2xl'>I want to become a seller</h1>
-            <div className="toggleSwitch flex items-center gap-2">
-            <label htmlFor="toggle" className='color-[gray] text-[18px]'>Activate the seller account</label>
-            <ToggleSwitch />
-            </div>
-            <label htmlFor="phone" className='color-[gray] text-[18px]'>Phone Number</label>
-            <input
-            className='p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200'
-              name="phone"
-              type="text"
-              id='phone'
-              placeholder="+1 234 567 89"
-             
-            />
-            <label htmlFor="desc" className='color-[gray] text-[18px]'>Description</label>
-            <textarea
-              placeholder="A short description of yourself"
-              name="desc"
-              id="desc"
-              cols="30"
-              rows="10"
-               className='min-h-[200px] p-[20px] border border-solid border-[lightgrey] rounded-md  shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200'
-            ></textarea>
-          </div>
-        </form>
-      </div>
-    );
-}
+          <label htmlFor="phone" className="color-[gray] text-[18px]">
+            Phone Number
+          </label>
+          <input
+            className="p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            name="phone"
+            type="text"
+            id="phone"
+            placeholder="+1 234 567 89"
+            onChange={handleChange}
+            value={user.phone}
+          />
+          <label htmlFor="desc" className="color-[gray] text-[18px]">
+            Description
+          </label>
+          <textarea
+            placeholder="A short description of yourself"
+            name="desc"
+            id="desc"
+            cols="30"
+            rows="10"
+            className="min-h-[200px] p-[20px] border border-solid border-[lightgrey] rounded-md  
+            shadow-sm appearance-none focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-200"
+            onChange={handleChange}
+            value={user.desc}
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
--- client/src/pages/register/toggleSwitch.jsx
+++ client/src/pages/register/toggleSwitch.jsx
@@ -1,35 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
-const ToggleSwitch = () => {
-  const [isToggled, setIsToggled] = useState(false);
-
-  const handleToggle = () => setIsToggled(!isToggled);
-
+const ToggleSwitch = ({ checked, onChange, id, name }) => {
   return (
     <div className="toggle flex items-center gap-2">
       <label className="switch relative inline-block w-14 h-7">
         <input
           type="checkbox"
-          checked={isToggled}
-          onChange={handleToggle}
+          onChange={onChange}     // Trigger parent-provided handler
           className="opacity-0 w-0 h-0"
+          id={id}                 // Set passed `id`
+          name={name}             // Set passed `name`
+          checked={checked}       // Controlled by parent
         />
         
         {/* Background Slider */}
         <span
           className={`slider absolute cursor-pointer inset-0 transition-colors duration-300 ease-in-out rounded-full ${
-            isToggled ? "bg-[#1dbf73]" : "bg-gray-300"
+            checked ? "bg-[#1dbf73]" : "bg-gray-300"
           }`}
         ></span>
         
         {/* Toggle Circle with Icon */}
         <span
           className={`absolute h-5 w-5 bg-white rounded-full flex items-center justify-center transition-transform duration-300 ease-in-out transform ${
-            isToggled ? "translate-x-8" : "translate-x-1"
+            checked ? "translate-x-8" : "translate-x-1"
           } bottom-1`}
         >
-          {isToggled ? (
+          {checked ? (
             <CheckIcon className="h-4 w-4 text-green-500" />
           ) : (
             <XMarkIcon className="h-4 w-4 text-red-500" />
