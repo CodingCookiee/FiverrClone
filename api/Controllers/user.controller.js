6332a2ec99a84594e0d55209c5b9bbab754971e2
CodingCookiee <rzqutubawan@gmail.com>
2024-11-14 17:48:09 +0500

    Updated Error handling

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -1,24 +1,25 @@
 import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
 
 export const deleteUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
-      return res.status(404).json({ message: "User not found!" });
+      return next(createError(404, "User not found!"))
     }
 
     if (!req.userId) {
-      return res.status(403).json({ message: "User ID is not set!" });
+      return next(createError(403, "You are not authenticated!"))
     }
 
     if (req.userId.toString() !== user._id.toString()) {
-      return res.status(403).json({ message: "You can delete only your account!" });
+      return next(createError(403, "You are not authorized to delete this user!"))
     }
 
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "User deleted successfully!" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    next(error);
   }
 };
