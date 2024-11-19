7a33e552c186ca57c2c1605e69e3f0b96d931e2d
CodePanda-crypto <rzqutubawan@gmail.com>
2024-11-19 22:27:10 +0500

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL: "https://fiverrclone.up.railway.app/api/",
   withCredentials: true,
+   SameSite = 'none',
+   secure = true
 });
 
 export default newRequest;
