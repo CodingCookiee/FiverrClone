fab84daae3b094dfdae923715e85e84cc8d4654a
CodingCookiee <rzqutubawan@gmail.com>
2024-11-20 00:37:34 +0500

    Update

--- client/src/pages/success/Success.jsx
+++ client/src/pages/success/Success.jsx
@@ -4,41 +4,44 @@ import { useEffect } from "react";
 
 const Success = () => {
   const { search } = useLocation();
+  const navigate = useNavigate();
   const params = new URLSearchParams(search);
   const payment_intent = params.get("payment_intent");
-  const navigate = useNavigate();
-
-
 
   useEffect(() => {
     const makeRequest = async () => {
       try {
+        if (!payment_intent) {
+          navigate("/");
+          return;
+        }
+        
         const response = await newRequest.put("/orders", { payment_intent });
-        if(response.status === 200) {
+        if (response.status === 200) {
           setTimeout(() => {
             navigate("/orders");
           }, 5000);
         }
       } catch (err) {
         console.log(err);
+        navigate("/");
       }
     };
-  
+
     makeRequest();
-  }, []);
-  
+  }, [payment_intent, navigate]);
 
-
-    return (
-      //  creative success page
-      <div className="success flex flex-col items-center justify-center h-screen">
+  return (
+    <div className="success flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold mb-4">Payment Successful!</h1>
       <p className="text-lg mb-4">
-      You will be redirected to the orders page in 5 seconds. If not, 
-      <a href="/orders" className="text-blue-500 hover:underline">click here</a>
+        You will be redirected to the orders page in 5 seconds. If not,{" "}
+        <a href="/orders" className="text-blue-500 hover:underline">
+          click here
+        </a>
       </p>
-      </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Success;
