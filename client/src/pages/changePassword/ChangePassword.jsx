cd02b4eb208817e01c5a81b0bb981ea7bcc10c0a
CodePanda-crypto <rzqutubawan@gmail.com>
2024-11-26 21:56:30 +0500

    Update ChangePassword.jsx

--- client/src/pages/changePassword/ChangePassword.jsx
+++ client/src/pages/changePassword/ChangePassword.jsx
@@ -21,13 +21,16 @@ const ChangePassword = () => {
   });
 
   const validatePassword = (password) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[a-zA-Z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+    
     if (password.length < 8) {
       return "Password must be at least 8 characters long";
     }
+    
     if (!passwordRegex.test(password)) {
-      return "Password must contain at least one uppercase letter, one lowercase letter and one number";
+      return "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character";
     }
+    
     return "";
   };
 
