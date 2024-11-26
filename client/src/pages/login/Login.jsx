6dfa80153fc573f8edfc74310dc426e2feac8428
CodePanda-crypto <rzqutubawan@gmail.com>
2024-11-26 21:50:25 +0500

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
           >
             Login
           </button>
-          {error && <span className="text-red-500">{error}</span>}
+          {error && <span className="text-red-500 self-center">{error}</span>}
           <Link to="/forgot-password" className="flex flex-col gap-5">
             <p className="text-[gray] text-[18-px] self-center">Forgot Password?</p>
           </Link>
