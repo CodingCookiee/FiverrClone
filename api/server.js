c37a50e290ee331905fbf4f89832f87a3ad7f46a
CodePanda-crypto <rzqutubawan@gmail.com>
2024-11-26 21:43:30 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -47,6 +47,13 @@ app.get('/health', (req, res) => {
   });
 });
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
 
 
 app.listen(port, async () => {
